c1153a888b6dd0851322cdbb437a532b22e824dd
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 13:20:32 +0100

    Initial commit

---
+++ ImageCoder/Dashboard.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ImageCoder
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.OpenFileDialog();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgBox
+            // 
+            this.imgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgBox.Location = new System.Drawing.Point(12, 95);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(695, 382);
+            this.imgBox.TabIndex = 0;
+            this.imgBox.TabStop = false;
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.Location = new System.Drawing.Point(12, 12);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(107, 23);
+            this.buttonDecode.TabIndex = 1;
+            this.buttonDecode.Text = "Decode Image";
+            this.buttonDecode.UseVisualStyleBackColor = true;
+            this.buttonDecode.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // open
+            // 
+            this.open.FileName = "openFileDialog1";
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(12, 41);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncode.TabIndex = 2;
+            this.buttonEncode.Text = "Encode Image";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            this.buttonEncode.Click += new System.EventHandler(this.ButtonEncode_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 489);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.buttonDecode);
+            this.Controls.Add(this.imgBox);
+            this.Name = "Dashboard";
+            this.Text = "iscord";
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.OpenFileDialog open;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ImageCoder/Dashboard.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCoder
+{
+    public partial class Dashboard : Form
+    {
+
+
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            open.ShowDialog();
+            var raw = new Bitmap(open.FileName);
+            Bitmap conv = Decode(raw);
+
+            imgBox.BackgroundImage = conv;
+        }
+
+        private static Bitmap Decode(Bitmap raw)
+        {
+            var conv = new Bitmap(raw.Width / 2, raw.Height / 2);
+
+            for (int x = 0; x < conv.Width; x++)
+            {
+                for (int y = 0; y < conv.Height; y++)
+                {
+                    conv.SetPixel(x, y, raw.GetPixel(x * 2 + 1, y * 2 + 1));
+                }
+            }
+
+            return conv;
+        }
+
+        private static Bitmap Encode(Bitmap original, Bitmap secret)
+        {
+            var raw = new Bitmap(original);
+            for (int x = 0; x < secret.Width; x++)
+            {
+                for (int y = 0; y < secret.Height; y++)
+                {
+                    raw.SetPixel(x * 2 +1, y * 2+1, secret.GetPixel(x, y));
+                }
+            }
+            return raw;
+        }
+
+        private void ButtonEncode_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openOrig = new OpenFileDialog();
+            OpenFileDialog openSecr = new OpenFileDialog();
+
+            openOrig.ShowDialog();
+            openSecr.ShowDialog();
+
+            var enc = Encode(new Bitmap(openOrig.FileName), new Bitmap(openSecr.FileName));
+            imgBox.BackgroundImage = enc;
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            var save = new SaveFileDialog();
+            save.ShowDialog();
+            new Bitmap(imgBox.BackgroundImage).Save(save.FileName);
+        }
+    }
+}
